70ee8376c94710172b5da6cc32a789b2bd25fd29
Emir <emirkan.ulker9@gmail.com>
2023-10-09 17:09:16 +0300

    NesneHareketler

---
+++ NesneHareketler/NesneHareketler/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace NesneHareketler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.panel11 = new System.Windows.Forms.Panel();
+            this.panel12 = new System.Windows.Forms.Panel();
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.timer6 = new System.Windows.Forms.Timer(this.components);
+            this.timer7 = new System.Windows.Forms.Timer(this.components);
+            this.timer8 = new System.Windows.Forms.Timer(this.components);
+            this.timer9 = new System.Windows.Forms.Timer(this.components);
+            this.timer10 = new System.Windows.Forms.Timer(this.components);
+            this.timer11 = new System.Windows.Forms.Timer(this.components);
+            this.timer12 = new System.Windows.Forms.Timer(this.components);
+            this.timer13 = new System.Windows.Forms.Timer(this.components);
+            this.timer14 = new System.Windows.Forms.Timer(this.components);
+            this.panel13 = new System.Windows.Forms.Panel();
+            this.timer15 = new System.Windows.Forms.Timer(this.components);
+            this.timer16 = new System.Windows.Forms.Timer(this.components);
+            this.panel14 = new System.Windows.Forms.Panel();
+            this.panel15 = new System.Windows.Forms.Panel();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(23, 693);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Location = new System.Drawing.Point(146, 189);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(40, 301);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Controls.Add(this.panel6);
+            this.panel3.Location = new System.Drawing.Point(146, 154);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(768, 39);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.White;
+            this.panel4.Location = new System.Drawing.Point(1045, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(23, 693);
+            this.panel4.TabIndex = 2;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.White;
+            this.panel5.Location = new System.Drawing.Point(898, 154);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(33, 333);
+            this.panel5.TabIndex = 2;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.White;
+            this.panel6.Location = new System.Drawing.Point(8, 8);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(768, 39);
+            this.panel6.TabIndex = 2;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.White;
+            this.panel7.Location = new System.Drawing.Point(307, 480);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(624, 10);
+            this.panel7.TabIndex = 2;
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.Color.White;
+            this.panel9.Location = new System.Drawing.Point(20, 664);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(1035, 29);
+            this.panel9.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 1;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::NesneHareketler.Properties.Resources.Moon_rotating_thirdquarter_220px;
+            this.pictureBox1.Location = new System.Drawing.Point(30, 587);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 71);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.White;
+            this.panel8.Location = new System.Drawing.Point(465, 272);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(315, 10);
+            this.panel8.TabIndex = 3;
+            // 
+            // panel10
+            // 
+            this.panel10.BackColor = System.Drawing.Color.White;
+            this.panel10.Location = new System.Drawing.Point(611, 272);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(172, 10);
+            this.panel10.TabIndex = 4;
+            // 
+            // panel11
+            // 
+            this.panel11.BackColor = System.Drawing.Color.White;
+            this.panel11.Location = new System.Drawing.Point(465, 275);
+            this.panel11.Name = "panel11";
+            this.panel11.Size = new System.Drawing.Size(32, 208);
+            this.panel11.TabIndex = 5;
+            // 
+            // panel12
+            // 
+            this.panel12.BackColor = System.Drawing.Color.White;
+            this.panel12.Location = new System.Drawing.Point(181, 370);
+            this.panel12.Name = "panel12";
+            this.panel12.Size = new System.Drawing.Size(163, 10);
+            this.panel12.TabIndex = 6;
+            // 
+            // timer5
+            // 
+            this.timer5.Interval = 1;
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick);
+            // 
+            // timer6
+            // 
+            this.timer6.Interval = 1;
+            this.timer6.Tick += new System.EventHandler(this.timer6_Tick);
+            // 
+            // timer7
+            // 
+            this.timer7.Interval = 1;
+            this.timer7.Tick += new System.EventHandler(this.timer7_Tick);
+            // 
+            // timer8
+            // 
+            this.timer8.Interval = 1;
+            this.timer8.Tick += new System.EventHandler(this.timer8_Tick);
+            // 
+            // timer9
+            // 
+            this.timer9.Interval = 1;
+            this.timer9.Tick += new System.EventHandler(this.timer9_Tick);
+            // 
+            // timer10
+            // 
+            this.timer10.Interval = 1;
+            this.timer10.Tick += new System.EventHandler(this.timer10_Tick);
+            // 
+            // timer11
+            // 
+            this.timer11.Interval = 1;
+            this.timer11.Tick += new System.EventHandler(this.timer11_Tick);
+            // 
+            // timer12
+            // 
+            this.timer12.Interval = 1;
+            this.timer12.Tick += new System.EventHandler(this.timer12_Tick);
+            // 
+            // timer13
+            // 
+            this.timer13.Interval = 1;
+            this.timer13.Tick += new System.EventHandler(this.timer13_Tick);
+            // 
+            // timer14
+            // 
+            this.timer14.Interval = 1;
+            this.timer14.Tick += new System.EventHandler(this.timer14_Tick);
+            // 
+            // panel13
+            // 
+            this.panel13.BackColor = System.Drawing.Color.White;
+            this.panel13.Location = new System.Drawing.Point(307, 272);
+            this.panel13.Name = "panel13";
+            this.panel13.Size = new System.Drawing.Size(167, 10);
+            this.panel13.TabIndex = 7;
+            // 
+            // timer15
+            // 
+            this.timer15.Interval = 1;
+            this.timer15.Tick += new System.EventHandler(this.timer15_Tick);
+            // 
+            // timer16
+            // 
+            this.timer16.Interval = 1;
+            this.timer16.Tick += new System.EventHandler(this.timer16_Tick);
+            // 
+            // panel14
+            // 
+            this.panel14.BackColor = System.Drawing.Color.White;
+            this.panel14.Location = new System.Drawing.Point(609, 381);
+            this.panel14.Name = "panel14";
+            this.panel14.Size = new System.Drawing.Size(174, 10);
+            this.panel14.TabIndex = 5;
+            // 
+            // panel15
+            // 
+            this.panel15.BackColor = System.Drawing.Color.White;
+            this.panel15.Location = new System.Drawing.Point(766, 272);
+            this.panel15.Name = "panel15";
+            this.panel15.Size = new System.Drawing.Size(17, 116);
+            this.panel15.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(1067, 692);
+            this.Controls.Add(this.panel15);
+            this.Controls.Add(this.panel14);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel13);
+            this.Controls.Add(this.panel10);
+            this.Controls.Add(this.panel12);
+            this.Controls.Add(this.panel11);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel9);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.Panel panel11;
+        private System.Windows.Forms.Panel panel12;
+        private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.Timer timer6;
+        private System.Windows.Forms.Timer timer7;
+        private System.Windows.Forms.Timer timer8;
+        private System.Windows.Forms.Timer timer9;
+        private System.Windows.Forms.Timer timer10;
+        private System.Windows.Forms.Timer timer11;
+        private System.Windows.Forms.Timer timer12;
+        private System.Windows.Forms.Timer timer13;
+        private System.Windows.Forms.Timer timer14;
+        private System.Windows.Forms.Panel panel13;
+        private System.Windows.Forms.Timer timer15;
+        private System.Windows.Forms.Timer timer16;
+        private System.Windows.Forms.Panel panel14;
+        private System.Windows.Forms.Panel panel15;
+    }
+}
+
---
+++ NesneHareketler/NesneHareketler/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NesneHareketler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+            // YUKARI
+        {
+            pictureBox1.Top -= 5;
+            if (pictureBox1.Top <= 22)
+            {
+                timer1.Stop();
+                timer2.Start();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+          
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+            // SAĞA
+        {
+            pictureBox1.Left += 5;
+            if(pictureBox1.Left >= 939)
+            {
+                timer2.Stop();
+                timer3.Start();
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+            // AŞAĞI
+        {
+            pictureBox1.Top += 5;
+            if(pictureBox1.Top >= 587)
+            {
+                timer3.Stop();
+                timer4.Start();
+            }
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+            // SOLA
+        {
+            pictureBox1.Left -= 5;
+            if(pictureBox1.Left <= 191)
+            {
+                timer4.Stop();
+                timer5.Start();
+            }
+        }
+
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Top -= 5;
+            if(pictureBox1.Top <= 388)
+            {
+                timer5.Stop();
+                timer6.Start();
+            }
+        }
+
+        private void timer6_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Left += 5;
+            if(pictureBox1.Left >= 359)
+            {
+                timer6.Stop();
+                timer7.Start();
+            }
+        }
+
+        private void timer7_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Top -= 5;
+            if(pictureBox1.Top <= 295)
+            {
+                timer7.Stop();
+                timer8.Start();
+            }
+        }
+
+        private void timer8_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Left -= 5;
+            if(pictureBox1.Left <= 192)
+            {
+                timer8.Stop();
+                timer9.Start();
+            }
+        }
+
+        private void timer9_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Top -= 5;
+            if(pictureBox1.Top <= 199)
+            {
+                timer9.Stop();
+                timer10.Start();
+            }
+        }
+
+        private void timer10_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Left += 5;
+            if(pictureBox1.Left >= 792)
+            {
+                timer10.Stop();
+                timer11.Start();
+            }
+        }
+
+        private void timer11_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Top += 5;
+            if(pictureBox1.Top >= 388)
+            {
+                timer11.Stop();
+                timer12.Start();
+            }
+        }
+
+        private void timer12_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Left -= 5;
+            if(pictureBox1.Left <= 503)
+            {
+                timer12.Stop();
+                timer13.Start();
+            }
+
+        }
+
+
+        private void timer13_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Top -= 5;
+            if(pictureBox1.Top <= 292)
+            {
+                timer13.Stop();
+                timer14.Start();
+            }
+        }
+
+        private void timer14_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Left += 5;
+            if(pictureBox1.Left >= 649)
+            {
+                timer14.Stop();
+                timer15.Start();
+            }
+        }
+
+        private void timer15_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Top += 5;
+            if(pictureBox1.Top >= 587)
+            {
+                timer15.Stop();
+                timer16.Start();
+            }
+        }
+
+        private void timer16_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Left -= 5;
+            if(pictureBox1.Left <= 30)
+            {
+                timer16.Stop();
+                MessageBox.Show("Dönme İşlemi Bitmiştir İşlem Tekrarlanıyor...");
+                timer1.Start();
+            }
+        }
+    }
+}
